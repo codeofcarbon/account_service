944f17bc128ecf4034c3041a92857dff429978bc
Krzysztof <code.of.carbon@gmail.com>
2021-12-12 00:04:17 +0100

    option for changing user password added, also password correctness verification and testing against dummy breached passwords

--- src/main/java/codeofcarbon/account/controller/AccountController.java
+++ src/main/java/codeofcarbon/account/controller/AccountController.java
@@ -5,9 +5,13 @@ import codeofcarbon.account.model.dto.UserDTO;
 import codeofcarbon.account.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api")
@@ -19,9 +23,23 @@ public class AccountController {
         this.userService = userService;
     }
 
+    //  new user registering
     @PostMapping("/signup")
     public ResponseEntity<UserDTO> signUp(@Validated @RequestBody User user) {
         var newUser = userService.addNewUser(user);
         return ResponseEntity.ok(UserDTO.mapToUserDTO(newUser));
+    }
+
+    //  changing user password after successful authentication
+    @PostMapping("/changepass")
+    public ResponseEntity<Object> changePassword(@AuthenticationPrincipal UserDetails details,
+                                                 @RequestBody Map<String, String> request) {
+        if (details == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        var newPassword = request.get("new_password");
+        userService.checkPasswordRequirements(newPassword, details.getPassword());
+        userService.updatePassword(newPassword, (User) details);
+        return ResponseEntity.ok(Map.of(
+                "email", details.getUsername(),
+                "status", "The password has been updated successfully"));
     }
 }
--- src/main/java/codeofcarbon/account/service/UserService.java
+++ src/main/java/codeofcarbon/account/service/UserService.java
@@ -6,14 +6,20 @@ import codeofcarbon.account.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2A, 13);
+    private final List<String> breachedPasswords =
+            List.of("PasswordForJanuary", "PasswordForFebruary", "PasswordForMarch", "PasswordForApril",
+                    "PasswordForMay", "PasswordForJune", "PasswordForJuly", "PasswordForAugust",
+                    "PasswordForSeptember", "PasswordForOctober", "PasswordForNovember", "PasswordForDecember");
 
     @Autowired
     public UserService(UserRepository userRepository) {
@@ -22,17 +28,35 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) {
-        return userRepository.findByEmail(email.toLowerCase())
-                .orElseThrow(() -> new UsernameNotFoundException(
-                        String.format("user with email %s doesn't exist", email)));
+        try {
+            return userRepository.findByEmail(email.toLowerCase()).orElseThrow(Exception::new);
+        } catch (Exception ignored) {
+                return null;
+        }
     }
 
+    @Transactional
     public User addNewUser(User user) {
         if (userRepository.findAll().stream()
-                .noneMatch(u -> user.getEmail().equalsIgnoreCase(u.getEmail())))
+                .anyMatch(u -> user.getEmail().equalsIgnoreCase(u.getEmail()))) new Exception();
+        var validPassword = checkPasswordRequirements(user.getPassword(), "");
+        user.setPassword(encoder.encode(validPassword));
         user.setEmail(user.getEmail().toLowerCase());
-        user.setPassword(encoder.encode(user.getPassword()));
         user.grantAuthority(Role.USER);
         return userRepository.save(user);
+    }
+
+    public String checkPasswordRequirements(String newPassword, String currentPassword) {
+        if (newPassword.length() < 12) new Exception();
+        if (breachedPasswords.contains(newPassword)) new Exception();
+        if (encoder.matches(newPassword, currentPassword)) new Exception();
+        return newPassword;
+    }
+
+    @Transactional
+    public void updatePassword(String newPassword, User user) {
+        var validPassword = checkPasswordRequirements(newPassword, user.getPassword());
+        user.setPassword(encoder.encode(validPassword));
+        userRepository.save(user);
     }
 }
