8ff4fcb0012854be53cb112cadd403d7b54b34f8
Krzysztof <code.of.carbon@gmail.com>
2022-05-23 17:54:00 +0200

    checkpoint 2

--- src/test/java/com/codeofcarbon/accountservicetests/AllTest.java
+++ src/test/java/com/codeofcarbon/accountservicetests/AllTest.java
@@ -1,13 +1,9 @@
 package com.codeofcarbon.accountservicetests;
 
 import com.codeofcarbon.accountservice.AccountServiceApplication;
-import org.hyperskill.hstest.dynamic.SystemHandler;
 import org.hyperskill.hstest.stage.SpringTest;
-import org.hyperskill.hstest.testcase.CheckResult;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 
 public class AllTest extends SpringTest {
     private final static String databasePath = "./src/main/resources/service_db.mv.db";
@@ -21,46 +17,14 @@ public class AllTest extends SpringTest {
         var loggingSecurityEventsTest = new LoggingSecurityEventsTest();
         businessLogicTest.start();
         businessLogicTest.stopSpring();
-
-//        businessLogicTest.reloadSpring();
-        deleteDatabaseFiles(Path.of(databasePath), Path.of(databasePath + "-real"));
-        loggingSecurityEventsTest.reloadSpring();
-//        businessLogicTest.tearDown();
-        loggingSecurityEventsTest.start();
-        businessLogicTest.stopSpring();
-
-//        try {
-//            SystemHandler.tearDownSystem();
-//        } catch (Throwable ignored) {
-//        }
-//        System.exit(0);
-//        this.reloadSpring();
-    }
-
-    private static void deleteDatabaseFiles(Path database, Path tempDatabase) {
         try {
-            Files.delete(database);
-            Files.delete(tempDatabase);
-//        } catch (NoSuchFileException e) {
-//            System.err.format("%s: no such" + " file or directory\n", database);
-//        } catch (DirectoryNotEmptyException e) {
-//            System.err.format("%s not empty\n", database);
-//        } catch (IOException e) {
-//            // =============================================================== file permission problems are caught here
-//            System.err.println(e.getMessage());
-//        }
-//        try {
-//            Files.delete(database);
-//        } catch (NoSuchFileException e) {
-//            System.err.format("%s: no such" + " file or directory\n", database);
-//        } catch (DirectoryNotEmptyException e) {
-//            System.err.format("%s not empty\n", database);
+            Files.delete(Path.of(databasePath));
+            Files.delete(Path.of(databasePath + "-real"));
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
-//        try {
-//            SystemHandler.tearDownSystem();
-//        } catch (Throwable ignored) {
-//        }
+        loggingSecurityEventsTest.reloadSpring();
+        loggingSecurityEventsTest.start();
+        loggingSecurityEventsTest.stopSpring();
     }
 }
